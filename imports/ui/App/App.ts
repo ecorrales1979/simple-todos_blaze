9c569cab7ece6d1f1410486d48297c4fc33876c4
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2025-06-13 23:42:22 -0300

    fet: create event listener to add task

--- imports/ui/App/App.ts
+++ imports/ui/App/App.ts
@@ -1,10 +1,27 @@
 import { Template } from 'meteor/templating';
 
 import { TaskCollection } from '../../api/tasks/TaskCollection';
+// import './App.styles.css';
 import './App.view.html';
 
 Template.mainContainer.helpers({
   tasks() {
     return TaskCollection.find();
+  },
+});
+
+Template.form.events({
+  'submit .task-form'(event: Event) {
+    event.preventDefault();
+
+    const target = event.target as HTMLFormElement;
+    const text = target.text.value;
+
+    TaskCollection.insert({
+      text,
+      createdAt: new Date(),
+    });
+
+    target.text.value = '';
   },
 });
