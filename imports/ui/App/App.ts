8e9856e0e47b7739b382a9f22e5aef14cfd6e054
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2025-06-18 23:51:10 -0300

    feat: add a filter button

--- imports/ui/App/App.ts
+++ imports/ui/App/App.ts
@@ -1,13 +1,46 @@
 import { Template } from 'meteor/templating';
 
+import { Blaze } from 'meteor/blaze';
+import { ReactiveDict } from 'meteor/reactive-dict';
 import { TaskCollection } from '../../api/tasks/TaskCollection';
 import { TaskFormElements } from '../../api/tasks/types';
 import '../components/Task';
 import './App.view.html';
 
+type MainContainerInstance = Blaze.TemplateInstance & {
+  state: ReactiveDict;
+};
+
+const HIDE_COMPLETED_STRING = 'hideCompleted';
+
+Template.mainContainer.onCreated(function () {
+  this.state = new ReactiveDict();
+});
+
 Template.mainContainer.helpers({
   tasks() {
-    return TaskCollection.find({}, { sort: { createdAt: -1 } });
+    const instance = Template.instance() as MainContainerInstance;
+    const hideCompleted = instance.state.get(HIDE_COMPLETED_STRING);
+
+    const hideCompletedFilter = { isChecked: { $ne: true } };
+
+    return TaskCollection.find(hideCompleted ? hideCompletedFilter : {}, {
+      sort: { createdAt: -1 },
+    }).fetch();
+  },
+  hideCompleted() {
+    const template = Template.instance() as MainContainerInstance;
+    return template.state.get(HIDE_COMPLETED_STRING);
+  },
+});
+
+Template.mainContainer.events({
+  'click #hide-completed-button': function (
+    event: Event,
+    instance: MainContainerInstance
+  ) {
+    const currentHideCompleted = instance.state.get(HIDE_COMPLETED_STRING);
+    instance.state.set(HIDE_COMPLETED_STRING, !currentHideCompleted);
   },
 });
 
